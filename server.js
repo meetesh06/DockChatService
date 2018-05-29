0b8e6e199780b5ca4d56553a733f9b9779e19e4e
Meetesh Mehta <meetesh@Meeteshs-MacBook-Air.local>
2018-05-29 22:51:59 +0530

    new structure for web and android

--- server.js
+++ server.js
@@ -14,18 +14,25 @@ const request = require('request');
 
 // This creates our socket using the instance of the server
 const io = socketIO(server)
-
-const globalManagedMemory = {}; // this will store the bulletins
-
+const globalManagedMemory = {};
 const SIZE_OF_CHANNEL = 250;
 
-// This is what the socket.io syntax is like, we will work this later
 io.on('connection', socket => {
-  console.log('New client connected')
-  
-  socket.on('bulletin subscription', (userRequest) => {
-    console.log(userRequest);
-    let user_request = JSON.parse(userRequest);
+  if(!socket.isAuthorized) {
+    
+    io.to(socket.id).emit('feedback', JSON.stringify({ error: false, data: 500 }) );
+  }
+
+  socket.on('auth request', (userRequest) => {
+    let user_request = {};
+    try {
+      user_request = JSON.parse(userRequest);
+    } catch(e) {
+      
+      io.to(socket.id).emit('feedback', JSON.stringify({ error: true, mssg: 'invalid auth request' }) );
+      socket.disconnect();
+      return;
+    }
     if(user_request.token && user_request.bulletins) {
       var options = {
         url: 'https://mycampusdock.com/verify-chat',
@@ -34,63 +41,99 @@ io.on('connection', socket => {
         }
       };
       request.post(options,
-          function (error, response, body) {
-            body = JSON.parse(body);
-            if(body && body.error == false){
-              socket.userEmail = body.data;
-              let i = 0;
-              let toSend = {};
-              for(i=0;i<user_request.bulletins.length;i++) {
-                socket.join(user_request.bulletins[i]);
-                console.log(globalManagedMemory[user_request.bulletins[i]]);
-                toSend[user_request.bulletins[i]] = globalManagedMemory[user_request.bulletins[i]] != undefined ? globalManagedMemory[user_request.bulletins[i]] : [];
-              }
-              io.to(socket.id).emit('am i connected', JSON.stringify({ error: false, data: toSend}) );
-              console.log('handshake complete');
-            } else {
-              console.log('handshake failed', body);
-            }
+        function (error, response, body) {
+          if(error) {
+            io.to(socket.id).emit('feedback', JSON.stringify({ error: true, data: 'auth failed token error'}) );
+            socket.disconnect();
           }
+          
+          body = JSON.parse(body);
+          if(body && body.error == false){
+            socket.userEmail = body.data;
+            socket.userName = body.name;
+            let i = 0;
+            user_request.bulletins = user_request.bulletins.split(',');
+            for(i=0;i<user_request.bulletins.length;i++) {
+              socket.join(user_request.bulletins[i]);
+            }
+            
+            io.to(socket.id).emit('feedback', JSON.stringify({ error: false, data: 101}) );
+            socket.isAuthorized = true;
+          } else {
+            io.to(socket.id).emit('feedback', JSON.stringify({ error: true, mssg: 'auth failed'}) );
+            socket.disconnect();
+          }
+        }
       );
+    } else {
+      io.to(socket.id).emit('feedback', JSON.stringify({ error: true, mssg: 'auth failed'}) );
+      socket.disconnect();
     }
-    
+  });
+
+  socket.on('subscribe to new', (userRequest) => {
+    if(socket.isAuthorized) {
+      socket.join(userRequest);
+      io.to(socket.id).emit('feedback', JSON.stringify({ error: false, data: 103}) );
+    } else {
+      io.to(socket.id).emit('feedback', JSON.stringify({ error: true, mssg: 'auth failed'}) );
+      socket.disconnect();
+    }
   });
 
   socket.on('send to bulletin', (userRequest) => {
-    let request = JSON.parse(userRequest);
+    let request = {};
+    try {
+      request = JSON.parse(userRequest);
+    } catch(e) {
+      
+      io.to(socket.id).emit('feedback', JSON.stringify({ error: true, mssg: 'invalid auth request' }) );
+      socket.disconnect();
+      return;
+    }
+    
     if(socket.rooms.hasOwnProperty(request.bulletin)) {
-      console.log('can send: '+socket.userEmail);
+      
+      let now = new Date();
       if (globalManagedMemory[request.bulletin] != undefined) {
-        console.log('using existing memory instance');
+      
         if(globalManagedMemory[request.bulletin].length >= SIZE_OF_CHANNEL ){
           globalManagedMemory[request.bulletin].shift();
-          globalManagedMemory[request.bulletin].push({ email: socket.userEmail, message: request.message, timestamp: new Date()});
+          globalManagedMemory[request.bulletin].push({ email: socket.userEmail, message: request.message, timestamp: now});
         } else {
-          globalManagedMemory[request.bulletin].push({ email: socket.userEmail, message: request.message, timestamp: new Date()});
+          globalManagedMemory[request.bulletin].push({ email: socket.userEmail, message: request.message, timestamp: now});
         }
       } else {
-        console.log('creating new memory instance');
+      
         globalManagedMemory[request.bulletin] = [];
-        globalManagedMemory[request.bulletin].push({ email: socket.userEmail, message: request.message, timestamp: new Date()});
-        // Object.seal(globalManagedMemory[request.bulletin]);
+        globalManagedMemory[request.bulletin].push({ email: socket.userEmail, message: request.message, timestamp: now, name: socket.userName});
       }
-      console.log('global memory ',globalManagedMemory[request.bulletin]);
-      io.to(request.bulletin).emit('message in bulletin', { bulletin: request.bulletin, data: globalManagedMemory[request.bulletin]});
+      
+      
+      io.to(request.bulletin).emit('new_message_in_bulletin', JSON.stringify({ error: false, email: socket.userEmail, bulletin: request.bulletin, data: request.message, timestamp: now}));
     } else {
-      console.log('cannot send');
+      
+      io.to(socket.id).emit('feedback', JSON.stringify({ error: true, mssg: 'auth failed'}) );
+      socket.disconnect();
     }
   });
-
-  socket.on('am i connected', () => {
-    if(socket.userEmail != undefined) {
-      io.to(socket.id).emit('am i connected', JSON.stringify({ error: false}) );
-    } else {
-      io.to(socket.id).emit('am i connected', JSON.stringify({ error: true, mssg: 'token was probably invalid' }) );
+  
+  socket.on('synchronize', (userRequest) => {
+    if(socket.isAuthorized) {
+      
+      if(socket.rooms.hasOwnProperty(userRequest)) {
+        let toSend = { error: false, data: 105, payload: { bulletin_id: userRequest, messages: globalManagedMemory[userRequest] }};
+      
+        io.to(socket.id).emit('feedback', JSON.stringify(toSend) );
+      } else {
+        io.to(socket.id).emit('feedback', JSON.stringify({ error: true, mssg: 'auth failed bulletin not subscribed'}) );
+        socket.disconnect();  
+      }
     }
   });
 
   socket.on('disconnect', () => {
-    console.log('user disconnected');
+    
   })
 })
 
